895b9e3ccc5335c8912516a522b3a544c8516ace
Anthony Chambers <antmx1@gmail.com>
2025-07-09 00:45:34 +0100

    Add and Delete jobs

--- src/App.jsx
+++ src/App.jsx
@@ -8,19 +8,42 @@ import JobAdd from './pages/JobAdd'
 import Job from './pages/Job'
 import jobLoader from './utils/jobLoader'
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path='/' element={<Main />}>
-      <Route path='/' element={<Home />} />
-      <Route path='/jobs' element={<Jobs />} />
-      <Route path='/job/:id' element={<Job />} loader={jobLoader} />
-      <Route path='/jobadd' element={<JobAdd />} />
-      <Route path='*' element={<NotFound />} />
-    </Route>
-  )
-)
-
 const App = () => {
+
+  const addJob = async (newJob) => {
+    //console.log("newJob:" + JSON.stringify(newJob));
+    const response = await fetch('/api/jobs', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(newJob)
+    });
+
+    return;
+  }
+
+  const deleteJob = async (jobId) => {
+    //console.log("deleteJob: " + jobId);
+    const response = await fetch(`/api/jobs/${jobId}`, {
+      method: 'DELETE'
+    });
+    //console.log("deleteJob:" + jobId + " deleted");
+    return;
+  }
+
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path='/' element={<Main />}>
+        <Route path='/' element={<Home />} />
+        <Route path='/jobs' element={<Jobs />} />
+        <Route path='/job/:id' element={<Job deleteJobFn={deleteJob} />} loader={jobLoader} />
+        <Route path='/jobadd' element={<JobAdd addJobFn={addJob} />} />
+        <Route path='*' element={<NotFound />} />
+      </Route>
+    )
+  )
+
   return <RouterProvider router={router} />
 }
 
--- src/pages/Job.jsx
+++ src/pages/Job.jsx
@@ -1,12 +1,23 @@
 import React from 'react'
-import { useParams, useLoaderData } from 'react-router-dom'
+import { useParams, useLoaderData, useNavigate } from 'react-router-dom'
 import jobLoader from '../utils/jobLoader'
 import { FaArrowLeft, FaMapMarker } from 'react-icons/fa'
 import { Link } from 'react-router-dom'
 
-const Job = () => {
-
+const Job = ({ deleteJobFn }) => {
+	
 	const job = useLoaderData();
+	const navigate = useNavigate()
+
+	const onDeleteClick = async (jobId) => {
+
+		const confirmDelete = window.confirm("Are you sure you want to delete this job?");
+		if (!confirmDelete)
+			return;
+
+		deleteJobFn(jobId);
+		navigate('/jobs');
+	}
 
 	return (
 		<>
@@ -26,14 +37,14 @@ const Job = () => {
 					<div className="grid grid-cols-1 md:grid-cols-70/30 w-full gap-6">
 						<main>
 							<div
-								class="bg-white p-6 rounded-lg shadow-md text-center md:text-left"
+								className="bg-white p-6 rounded-lg shadow-md text-center md:text-left"
 							>
 								<div className="text-gray-500 mb-4">{job.type}</div>
 								<h1 className="text-3xl font-bold mb-4">
-									{ job.title }
+									{job.title}
 								</h1>
 								<div
-									class="text-gray-500 mb-4 flex align-middle justify-center md:justify-start"
+									className="text-gray-500 mb-4 flex align-middle justify-center md:justify-start"
 								>
 									<FaMapMarker className="mr-1 text-orange-700" />
 									<p className="text-orange-700">{job.location}</p>
@@ -85,10 +96,13 @@ const Job = () => {
 								<h3 className="text-xl font-bold mb-6">Manage Job</h3>
 								<Link
 									to={`/jobs/edit/${job.id}`}
-									class="bg-indigo-500 hover:bg-indigo-600 text-white text-center font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block">
+									className="bg-indigo-500 hover:bg-indigo-600 text-white text-center font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block">
 									Edit Job</Link>
 								<button
-									class="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block">
+									className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
+									onClick={() => {
+										onDeleteClick(job.id)
+									}}>
 									Delete Job
 								</button>
 							</div>
